4d9404a5c62642a3cc00b4d98b3fa999ddc517ef
Roeland <roeland@inkredible.nl>
2016-02-10 23:53:33 +0100

    added HateoasContainerFactory

---
+++ src/FluentHateoas/Registration/HateoasContainerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentHateoas.Registration
+{
+    public static class HateoasContainerFactory
+    {
+        public static HateoasContainer Create()
+        {
+            return new HateoasContainer(new HateoasConfiguration());
+        }
+    }
+}
