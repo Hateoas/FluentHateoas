5b262866d93736879151578e8c3aeb0976a1e526
Roeland <roeland@inkredible.nl>
2016-10-15 20:26:46 +0200

    [FLUEN-12] Added TemplateHandler

---
+++ src/FluentHateoas/Builder/Handlers/TemplateHandler.cs
@@ -0,0 +1,20 @@
+﻿using FluentHateoas.Handling;
+using FluentHateoas.Interfaces;
+
+namespace FluentHateoas.Builder.Handlers
+{
+    public class TemplateHandler : RegistrationLinkHandlerBase
+    {
+        public override LinkBuilder Process<TModel>(IHateoasRegistration<TModel> definition, LinkBuilder resourceBuilder, TModel data)
+        {
+            resourceBuilder.IsTemplate = definition.Expression.Template;
+
+            return base.Process(definition, resourceBuilder, data);
+        }
+
+        public override bool CanProcess<TModel>(IHateoasRegistration<TModel> registration, LinkBuilder resourceBuilder)
+        {
+            return true;
+        }
+    }
+}
