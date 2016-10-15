4dbabf23be5c117c46d77537a6f680a2ede432d3
Roeland Wemmenhove <roeland@inkredible>
2016-10-15 12:56:55 +0200

    [FLUEN-12] UseHandlerTest

---
+++ src/FluentHateoas/Builder/Handlers/UseHandler.cs
@@ -0,0 +1,19 @@
+﻿using FluentHateoas.Handling;
+using FluentHateoas.Interfaces;
+
+namespace FluentHateoas.Builder.Handlers
+{
+    public class UseHandler : RegistrationLinkHandlerBase
+    {
+        public override LinkBuilder Process<TModel>(IHateoasRegistration<TModel> definition, LinkBuilder resourceBuilder, TModel data)
+        {
+            return base.Process(definition, resourceBuilder, data);
+        }
+
+        public override bool CanProcess<TModel>(IHateoasRegistration<TModel> definition, LinkBuilder resourceBuilder)
+        {
+            return definition.Expression.Controller != null
+                   && (definition.Expression.TargetAction != null || definition.Expression.HttpMethod != null);
+        }
+    }
+}
